855789df98373850afce51760d88c4cc1c411325
Tulasi-R <58682084+Tulasi-R@users.noreply.github.com>
2020-03-13 16:41:58 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Input from './components/ToDo'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Input/>
+    
+    </div>
+  );
+}
+
+export default App;
